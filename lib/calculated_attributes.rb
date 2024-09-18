3d9fe0c99070c61de634b7235044200d61b65a65
adi-herwana-nus <adi.hwn@nus.edu.sg>
2024-09-18 15:29:51 +0800

    fix: create new patch for Rails 7.2 compatibility

--- lib/calculated_attributes.rb
+++ lib/calculated_attributes.rb
@@ -7,7 +7,11 @@ require 'calculated_attributes/arel_patches'
 
 raise "Unsupported ActiveRecord version: #{ActiveRecord::VERSION::MAJOR}" unless [6, 7].include? ActiveRecord::VERSION::MAJOR
 
-require "calculated_attributes/rails_#{ActiveRecord::VERSION::MAJOR}_patches"
+if ActiveRecord.version >= Gem::Version.new("7.2") && ActiveRecord.version < Gem::Version.new("8.0")
+  require "calculated_attributes/rails_7_2_patches"
+else
+  require "calculated_attributes/rails_#{ActiveRecord::VERSION::MAJOR}_patches"
+end
 
 # Include model code.
 require 'calculated_attributes/model_methods'
